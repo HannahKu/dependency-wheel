8fcdbfdbe9ee4b0d5ae419dc8fa1e262215daca1
ymugeny <106079433+ymugeny@users.noreply.github.com>
2022-06-07 15:29:35 +0200

    Update wheel.js

--- wheel.js
+++ wheel.js
@@ -14,7 +14,7 @@ var dataset = [
     [0,  1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0],//5 Brugg Cables
     [1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],//6 Sichtbarkeit
     [0, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],//7 Veranstaltungen
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],//8 Forschungsprojekte
+    [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],//8 Forschungsprojekte
     [0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0],//9 Praxis integriertes Studium
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1],//10 Studierenden Projekte
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0],//11 Nachhaltigkeit
