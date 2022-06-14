c63b5956a60ef070f2f769476bffb94bae006e90
ymugeny <106079433+ymugeny@users.noreply.github.com>
2022-06-14 09:32:22 +0200

    Update wheel.js

--- wheel.js
+++ wheel.js
@@ -7,18 +7,26 @@ var width = 1000,
 //relation matrix
 //EDIT
 var dataset = [
-    [0,  1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],//1 Institut für Elektrische Energietechnik FHNW
-    [1,  0, 0, 1, 1, 0, 1, 0, 0, 1, 0, 0, 0],//2 Hochschule für Technik FHNW
-    [0,  0, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0],//3 Weitere Hochschulpartner FHNW
-    [0,  1, 1, 0, 0, 0, 0, 0, 1, 1, 0, 1, 0],//4 Pfiffner 
-    [0,  1, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0],//5 Moser Glaser
-    [1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],//6 Alpha-ET
-    [0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],//7 Haefely
-    [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1],//8 Haveco
-    [0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0],//9 Pfiffner Systems
-    [0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1],//10 Brugg Cables
-    [0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0],//11 Gut eingespielte Zusammenarbeit
-    [0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0],//12 Beherbung Fachkräftemangel
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],//1 Institut für Elektrische Energietechnik FHNW
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],//2 Hochschule für Technik FHNW
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],//3 Weitere Hochschulpartner FHNW
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],//4 Pfiffner 
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],//5 Moser Glaser
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],//6 Alpha-ET
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],//7 Haefely
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],//8 Haveco
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],//9 Pfiffner Systems
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],//10 Brugg Cables
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],//11 Gut eingespielte Zusammenarbeit
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],//12 Behebung Fachkräftemangel
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],//13 Sichtbarkeit
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],//14 Veranstaltungen
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],//15 Forschungsprojekte
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],//16 Praxisintegriertes Studium
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],//17 Studierendenprojekte
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],//18 Elektrische Energiespeicherung
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],//19 Nachhaltige Energieversorgung
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],//20 Elektrische Netze der Zukunft
   ];
 
 //create the arc path data generator for the groups
