2364dd1d7999a2da599b0d3be24f7bf435b1fc25
ymugeny <106079433+ymugeny@users.noreply.github.com>
2022-07-01 15:09:07 +0200

    Update wheel.js

--- wheel.js
+++ wheel.js
@@ -7,28 +7,28 @@ var width = 1000,
 //relation matrix
 //EDIT
 var dataset = [
-  [0,	0, 0,	1, 1,	1, 1,	1, 1,	1, 1, 1, 0,	0, 1,	1, 1,	1, 0,	0, 0,	1],// 1 Institut für Elektrische Energietechnik FHNW
+  [0,	0, 0,	1, 1,	1, 1,	1, 1,	1, 1, 1, 0,	0, 1,	1, 1,	1, 0,	0, 1,	1],// 1 Institut für Elektrische Energietechnik FHNW
   [0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1],// 2 Hochschule für Technik FHNW 
   [0, 0, 0,	1, 1,	1, 1,	1, 1,	1, 1,	0, 0,	0, 1,	1, 1,	0, 1,	0, 0, 0],// 3 Weitere Hochschulpartner FHNW
   [1, 1, 1,	0, 0,	0, 0,	0, 0,	0, 0,	0, 0,	1, 0,	0, 1,	1, 0,	1, 1,	1],// 4 Pfiffner
   [1,	1, 1,	0, 0, 0, 0,	0, 0,	0, 0,	0, 0,	0, 0,	1, 1,	1, 0,	0, 0, 0],// 5 Moser Glaser
   [1,	1, 1,	0, 0,	0, 0,	0, 0,	0, 0,	0, 0,	0, 0,	0, 0,	0, 0,	0, 0,	0],// 6 Alpha-ET
-  [1,	1, 1,	0, 0,	0, 0,	0, 0,	0, 0,	0, 0,	0, 0,	0, 0,	0, 0,	0, 0,	0],// 7 Haefely
-  [1, 1, 1,	0, 0,	0, 0,	0, 0,	0, 0,	0, 0,	0, 0,	0, 1,	0, 0,	1, 0,	0],// 8 Haveco 
-  [1,	1, 1,	0, 0,	0, 0, 0, 0,	0, 0,	0, 0,	0, 0,	0, 1,	1, 1,	1, 1,	0],// 9 Brugg Cables
+  [1,	1, 1,	0, 0,	0, 0,	0, 0,	0, 0,	0, 0,	0, 0,	0, 0,	0, 0,	0, 0,	1],// 7 Haefely
+  [1, 1, 1,	0, 0,	0, 0,	0, 0,	0, 0,	0, 0,	0, 0,	0, 1,	0, 0,	1, 0,	1],// 8 Haveco 
+  [1,	1, 1,	0, 0,	0, 0, 0, 0,	0, 0,	0, 0,	0, 0,	0, 1,	1, 1,	1, 1,	1],// 9 Brugg Cables
   [1,	0, 1,	0, 0,	0, 0,	0, 0,	0, 0,	0, 1,	1, 0,	0, 0,	0, 0,	0, 0,	0],// 10 Gut eingespielte Zusammenarbeit
   [1,	0, 1,	0, 0,	0, 0,	0, 0,	0, 0,	0, 1,	1, 0,	0, 0,	0, 0,	0, 0,	0],// 11 Gewinnung von Fachkräften
   [1,	1, 0,	0, 0,	0, 0,	0, 0,	0, 0,	0, 1,	1, 0,	0, 1,	0, 0,	1, 1,	1],//12 Innovationsförderung
   [0,	1, 0,	0, 0,	0, 0,	0, 0,	1, 1,	1, 0,	0, 0,	0, 0,	0, 0,	0, 0,	0],//13 Weiterentwicklung Expertise
   [0,	1, 0,	1, 0,	0, 0,	0, 0,	1, 1,	1, 0,	0, 0,	0, 0,	0, 0,	0, 0,	0],//14 Stärkung der Standorte
   [1,	1, 1,	0, 0,	0, 0,	0, 0,	0, 0,	0, 0,	0, 0,	0, 0,	0, 0,	0, 0,	0],//15 Gemeinsame Nutzung von Infrastrukturen 
-  [1,	0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],//16 Austausch und Veranstaltungen
+  [1,	0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],//16 Austausch und Veranstaltungen
   [1, 1, 1,	1, 1,	0, 0,	1, 1,	0, 0,	1, 0,	0, 0,	0, 0,	0, 0,	1, 1,	1],//17 Forschungsprojekte
   [1,	1, 0, 1, 1,	0, 0,	0, 1,	0, 0,	0, 0,	0, 0,	0, 0,	0, 0, 0, 0,	0],//18 Praxisintegriertes Studium
   [0, 0, 1,	0, 0,	0, 0,	0, 1,	0, 0,	0, 0,	0, 0,	0, 0,	0, 0,	1, 1,	1],//19 Studierendenprojekte
   [0,	1, 0,	1, 0,	0, 0,	1, 1,	0, 0,	1, 0,	0, 0,	0, 1,	0, 1,	0, 0,	0],//20 Elektrische Energiespeicherung
-  [0,	1, 0,	1, 0,	0, 0,	0, 1,	0, 0,	1, 0,	0, 0,	0, 1,	0, 1,	0, 0,	0],//21 Nachhaltige Energieversorgung
-  [1,	1, 0,	1, 0,	0, 0,	0, 0,	0, 0,	1, 0,	0, 0,	0, 1,	0, 1,	0, 0,	0],//22 Elektrische Netze der Zukunft 
+  [1,	1, 0,	1, 0,	0, 0,	0, 1,	0, 0,	1, 0,	0, 0,	0, 1,	0, 1,	0, 0,	0],//21 Nachhaltige Energieversorgung
+  [1,	1, 0,	1, 0,	0, 1,	1, 1,	0, 0,	1, 0,	0, 0,	1, 1,	0, 1,	0, 0,	0],//22 Elektrische Netze der Zukunft 
   
 
 ];
